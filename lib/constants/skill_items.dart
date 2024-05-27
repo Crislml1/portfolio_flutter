71d12de77ade9477172566fbcf0d024b000a6474
ChristopherV654 <cvasco@mardisresearch.com>
2024-05-27 17:03:23 -0500

    Feat: Seccion de Skills agregada

---
+++ lib/constants/skill_items.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const List<Map> plataformsItems = [
+  {'img': Icon(Icons.android_rounded), 'title': 'Android Developer'},
+  {'img': Icon(Icons.web_rounded), 'title': 'Web Developer'},
+  {'img': Icon(Icons.apple_rounded), 'title': 'iOS Developer'},
+  {'img': Icon(Icons.category_rounded), 'title': 'UX/UI Designer'},
+];
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter/constants/colors.dart';
+import 'package:portfolio_flutter/constants/skill_items.dart';
 import 'package:portfolio_flutter/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -36,10 +37,55 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           //* SKILLS
           Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-            child: const Text('Empieza skills'),
+            width: sizeWidth,
+            padding: const EdgeInsets.only(bottom: 60, left: 25),
+            color: CustomColors.bgLight1,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'What I can do',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: CustomColors.whitePrimary,
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        maxWidth: 450,
+                      ),
+                      child: Wrap(
+                        spacing: 5,
+                        runSpacing: 5,
+                        children: [
+                          for (int i = 0; i < plataformsItems.length; i++)
+                            Container(
+                              width: 220,
+                              decoration: BoxDecoration(
+                                color: CustomColors.bgLight2,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: ListTile(
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                  vertical: 10,
+                                ),
+                                leading: plataformsItems[i]['img'],
+                                title: Text(plataformsItems[i]['title']),
+                              ),
+                            ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
           //* PROJECTS
           Container(
