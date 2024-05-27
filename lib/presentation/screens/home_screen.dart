394dd9cadefe247213887ea96b54ebe90ec980d9
Christopher V. <christopher.2001v@outlook.es>
2024-05-26 20:35:31 -0500

    Feat: Pantalla HomeScreen creada y colores para la pantalla agregados

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_flutter/constants/colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
+    return Scaffold(
+      backgroundColor: CustomColors.scaffoldBg,
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          //* MAIN
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+            child: Text('Empieza el main'),
+          ),
+          //* SKILLS
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+            child: Text('Empieza skills'),
+          ),
+          //* PROJECTS
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+            child: Text('Empieza projects'),
+          ),
+          //* CONTACT
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+            child: Text('Empieza el contact'),
+          ),
+          //* FOOTER
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+            child: Text('Empieza el footer'),
+          ),
+        ],
+      )
     );
   }
 }
